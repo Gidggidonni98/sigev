c27a6941352293e33b6397c8de91a71a1cd5f46c
Alan Yahir <alan.yahir.borjes@gmail.com>
2023-03-17 10:34:45 -0600

    [Update] se actualizo los CDN a Estaticos y modificacione en login

--- src/main/java/utez/edu/mx/sigev/SigevApplication.java
+++ src/main/java/utez/edu/mx/sigev/SigevApplication.java
@@ -9,5 +9,5 @@ public class SigevApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SigevApplication.class, args);
 	}
-
+	
 }
--- src/main/java/utez/edu/mx/sigev/controller/LoginController.java
+++ src/main/java/utez/edu/mx/sigev/controller/LoginController.java
@@ -1,4 +1,14 @@
 package utez.edu.mx.sigev.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class LoginController {
+    @GetMapping(value="/Login")
+    public String login() {
+        return  "login";
+    }
+    
 }
